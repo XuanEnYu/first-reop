3a5d60775f4c5475f566c12c816dc5a9ecab5714
ChaYeDan <3451247408@qq.com>
2024-06-17 18:47:50 +0800

    更改

---
+++ Learn/PHP-learn/24/6.13,17/demo/dl.php
@@ -0,0 +1,15 @@
+<?php
+    require "conn.php";
+    $name = $_POST['username'];
+    $password = $_POST['password'];
+    if($name&&$password){
+        $sql = "select username,password from user where username='$name' and password ='$password'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_num_rows($result);
+        if(!$row){
+            echo"<script>alert('账户或密码错误，请重新登录');location='demo.html'</script>";
+        }else{
+            echo"<script>alert('登陆成功，欢迎回来');location='demo.html'</script>";
+        }
+    }
+?>
